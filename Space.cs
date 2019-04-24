244227c2064f9c49858d7061701fe7ca67532fa1
acrap5 <35173755+acrap5@users.noreply.github.com>
2019-04-24 13:25:53 -0400

    Add files via upload

---
+++ Space.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+public class Space
+{
+    Tile currentTile;
+    bool empty;
+    string type;
+    public int owner;
+	public Space(string type)
+	{
+        this.currentTile = null;
+        this.empty = true;
+        this.type = type;
+        this.owner = 0;
+	}
+
+    public void setTile(Tile piece)
+    {
+        this.currentTile = piece;
+        this.owner = piece.owner;
+    }
+
+    public bool isEmpty()
+    {
+        return this.empty;
+    }
+
+    public int pieceVal()
+    {
+        return (this.currentTile.mobility * this.owner);
+    }
+}
---
+++ Tile.cs
@@ -0,0 +1,95 @@
+﻿using System;
+
+public class Tile
+{
+    public int mobility, owner;
+    string color;
+
+    public Tile(int val, int owner, string color)
+    {
+        this.mobility = val;
+        this.owner = owner;
+        this.color = color;
+    }
+
+    public bool checkHarmony(Tile other)
+    {
+        if (this.color == "red")
+        {
+            if (this.mobility == 4)
+            {
+                if (other.color == "red")
+                {
+                    if (other.mobility == 4) return false;
+                    else return true;
+                }
+                else return false;
+            }
+            else if (this.mobility == 3)
+            {
+                if (other.color == "white")
+                {
+                    if (other.mobility == 5) return true;
+                    else return false;
+                }
+                else
+                {
+                    if (other.mobility == 4) return true;
+                    else return false;
+                }
+            }
+            else
+            {
+                if (other.color == "white")
+                {
+                    if (other.mobility == 3) return true;
+                    else return false;
+                }
+                else
+                {
+                    if (other.mobility == 4) return true;
+                    else return false;
+                }
+            }
+        }
+        else
+        {
+            if (this.mobility == 4)
+            {
+                if (other.color == "white")
+                {
+                    if (other.mobility == 4) return false;
+                    else return true;
+                }
+                else return false;
+            }
+            else if (this.mobility == 3)
+            {
+                if (other.color == "red")
+                {
+                    if (other.mobility == 5) return true;
+                    else return false;
+                }
+                else
+                {
+                    if (other.mobility == 4) return true;
+                    else return false;
+                }
+            }
+            else
+            {
+                if (other.color == "red")
+                {
+                    if (other.mobility == 3) return true;
+                    else return false;
+                }
+                else
+                {
+                    if (other.mobility == 4) return true;
+                    else return false;
+                }
+            }
+        }
+    }
+}
+
