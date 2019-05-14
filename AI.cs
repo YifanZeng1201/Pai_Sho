746b54bf916240622ce58e5ed0ae2c93e5b242ae
Yifan Zeng <yzeng@oberlin.edu>
2019-05-13 23:35:37 -0400

    just store it for a while

--- AI.cs
+++ AI.cs
@@ -1,5 +1,9 @@
-﻿using System;
-using System.Collections;
+﻿/* Author: Yifan Zeng
+ * Updated 5.6.2019
+ */
+
+using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
@@ -13,7 +17,7 @@ namespace Paisho
 
         public int Minimax(int depth, Treenode<int> root, bool isMaxPlayer, int alpha, int beta)
         {
-            if (depth == 3)
+            if (depth == 2)
             {
                 return root.Value;
             }
@@ -51,9 +55,49 @@ namespace Paisho
         }
 
         // development in progress
-        public Treenode<int> gameTree()
+        public Treenode<int> buildGameTree(Board board)
         {
             Tree<int> gameTree = new Tree<int>(0);
+            List<Space> PossTile = board.occupied;
+            List<Tile> currentTiles = new List<Tile>();
+            List<Tile> oppTiles = new List<Tile>();
+            foreach (Space s in PossTile)
+            {
+                Tile t = s.getTile();
+                if (t.owner == -1 && t.isFlower())
+                {
+                    currentTiles.Add(t);
+                }
+                if (t.owner == 1 && t.isFlower())
+                {
+                    oppTiles.Add(t);
+                }
+            }
+            foreach (Tile t1 in currentTiles)
+            {
+                //Board cpy= board.copy
+                List<Space> moves = cpy.poss_moves(t1);
+                foreach (Space m in moves)
+                {
+
+                    m.setTile(t1);
+                    int value = Eval.evaluation(cpy);
+                    Treenode<int> child1 = new Treenode<int>(value);
+                    gameTree.Root.AddChild(child1);
+                    foreach (Tile t2 in oppTiles)
+                    {
+                        // Board cpy2= cpy.copy
+                        List<Space> oppMoves = cpy2.poss_move(t2);
+                        foreach (Space o in oppMoves)
+                        {
+                            o.setTile(t2);
+                            int oppValue = Eval.evaluation(cpy2);
+                            Treenode<int> child2 = new Treenode<int>(oppValue);
+                            child1.AddChild(child2);
+                        }
+                    }
+                }
+            }
             return gameTree.Root;
         }
     }
